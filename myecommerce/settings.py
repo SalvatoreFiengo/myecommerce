4e42940b12f51fc58c854589902e56b17993d36f
SalvatoreFiengo <sal.fiengo@gmail.com>
2020-01-21 13:29:48 +0000

    added if not database url in environ sqlite database for travis

--- myecommerce/settings.py
+++ myecommerce/settings.py
@@ -85,15 +85,19 @@ WSGI_APPLICATION = 'myecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+
+if "DATABASE_URL" in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    print("Database URL not found. Using SQLite instead")
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -132,12 +136,12 @@ USE_L10N = False
 
 USE_TZ = True
 
-DATE_FORMAT ='j N Y' 
+DATE_FORMAT = 'j N Y'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 AWS_S3_OBJECT_PARAMETERS = {
-    'Expires':'Thu, 31 Dec 2099 20:00:00 GMT',
+    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
     'CacheControl': 'max-age=94608000',
 }
 
@@ -152,7 +156,7 @@ AWS_DEFAULT_ACL = None
 STATICFILES_LOCATION = 'static'
 STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 STATIC_URL = '/static/'
-STATICFILES_DIRS =(os.path.join(BASE_DIR, 'static'), ) 
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
 MEDIAFILES_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
